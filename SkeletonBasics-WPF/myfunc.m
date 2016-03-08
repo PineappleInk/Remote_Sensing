cbc4c23ae2e0e1e4f0b3c540557ef1e2c1b959d5
Justus Johansson Lindkvist <jusli087@student.liu.se>
2016-03-08 09:50:03 +0100

    test

--- SkeletonBasics-WPF/myfunc.m
+++ SkeletonBasics-WPF/myfunc.m
@@ -1,3 +1,11 @@
 function [x] = myfunc(a) 
+dif = mean(a);
 x = plot(a);
+t = text(mean(a), 150, 'Subject is moving, waiting for stabilizing', 'Color', 'none', 'Fontsize', 16);
+
+if (max(a)-min(a) >= 0.5)
+{
+	t.Color = 'red'
+}
+ylim([mean(a)-0.01 mean(a)+0.01])
 end
