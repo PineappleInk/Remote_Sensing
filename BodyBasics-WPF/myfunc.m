bf7641063717f092d5d979ae53e9cdc8d96f38eb
Simon Karlsson <simka427@student.liu.se>
2016-03-08 13:56:07 +0100

    myfunc fix

--- BodyBasics-WPF/myfunc.m
+++ BodyBasics-WPF/myfunc.m
@@ -2,12 +2,11 @@ function [x] = myfunc(a)
 x = plot(a);
 
 %KASS KOD
-color = 'green';
-%if (max(a)-min(a)) >= 0.01
-%	color = 'red';
-%
-%text(100, mean(a), 'Subject is moving, waiting for stabilizing', 'Color', color, 'Fontsize', 16);
-%SLUT PÅ KASS KOD
+color = 'none';
+if (max(a)-min(a)) >= 0.05
+	color = 'red';
+
+text(100, mean(a), 'Subject is moving, waiting for stabilizing', 'Color', color, 'Fontsize', 32);
 
 title('Plot av "Spine"-djup')
 xlabel('Tid/"frames"'), ylabel('Djupvärden [m]');
