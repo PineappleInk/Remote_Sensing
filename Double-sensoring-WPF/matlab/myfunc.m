7fa985dd950ff9b26576c2a991f31106e3c57e97
Lina Gunnarsson <lingu459@student.liu.se>
2016-03-14 09:39:12 +0100

    Added function for pulse from matlab.

---
+++ Double-sensoring-WPF/matlab/myfunc.m
@@ -0,0 +1,28 @@
+function [x] = myfunc(a) 
+%x = plot(a);
+
+%Spara en bra mätning
+matning3 = a;
+save matning3.mat
+
+x=plot(a)
+
+
+% %KASS KOD
+% color = 'none';
+% if (max(a)-min(a)) >= 0.05
+% 	color = 'red';
+% 
+% %text(100, float(mean(a)), 'Subject is moving, waiting for stabilizing', 'Color', color, 'Fontsize', 32);
+% 
+% title('Plot av "Spine"-djup')
+% xlabel('Tid/"frames"'), ylabel('Djupvärden [m]');
+% meanvalue = mean(a);
+% ylim([min(a)-0.05 max(a)+0.05]);
+% xlim([0 300]);
+% 
+% %answer_vector=breathing_instant(a);
+% %x=answer_vector;
+% 
+
+end
---
+++ Double-sensoring-WPF/matlab/pulse_instant.m
@@ -0,0 +1,67 @@
+function [ mean_pulse ] = pulse_instant( color_list )
+% Calculates mean pulse over some time-interval
+
+load mtlb
+
+% Inställningar; välj dina inställnignar för koden här
+samples_per_sec = 30;
+% Slut inställningar
+
+% Info om mätdata
+number_of_samples = length(color_list);
+time_of_measurement = number_of_samples/samples_per_sec;
+% Slut på info om mätdata
+
+% Gör längden på vectorns indata udda
+if (mod(number_of_samples,2) == 0) % color_list är jämn i längd
+    color_list = color_list(2:number_of_samples);
+    number_of_samples = length(color_list)
+end
+% Slut
+
+% Plot av rådata
+figure(2)
+subplot(2,1,1)
+plot(color_list);
+grid on
+%grid minor
+title('Plot puls rådata');
+xlabel('tid [s/30]')
+ylabel('Färgskillnad i pixel i ansiktet (röd-grön)')
+
+% Filtrera color_list med hjälp av högsta möjliga gradens
+% Savitzky Golay FIR-filter
+if length(color_list)>=3
+    
+    color_list=double(color_list); % Kanske ej behövs
+    degree_of_polynomial = samples_per_sec - 1;
+    smooth_color_list = sgolayfilt(color_list,degree_of_polynomial,number_of_samples);
+    
+    % Lokaliserar peakarna i den filtrerade kurvan
+    %[~, peak_location]=findpeaks(smooth_color_list, 'MinPeakDistance',10);
+    [~, peak_location]=findpeaks(smooth_color_list)
+    number_of_peaks=length(peak_location);
+end
+% Slut filtrering
+
+% Pulsens medelvärde över antal sekunder
+BPM_pulse = (number_of_peaks/time_of_measurement)*60;
+% Slut medelvärde över antal sekunder
+
+% Plot av filtrerad data
+%figure(2)
+subplot(2,1,2)
+plot(smooth_color_list);
+grid on
+%grid minor
+title('Plot puls filtrerad med Savitzky Golay av högsta möjliga grad');
+xlabel('tid [s/30]')
+ylabel('Färgskillnad i pixel i ansiktet (röd-grön)')
+% Slut plot av filtrerad data
+
+% Utskrifter
+BPM_pulse
+% Slut utskrifter
+
+end
+
