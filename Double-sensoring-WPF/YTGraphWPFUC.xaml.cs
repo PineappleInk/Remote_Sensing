920d138026aeef711727578834e0bb9523247b63
rasgu789 <rasmusgustavsson.rg@gmail.com>
2016-04-12 09:17:47 +0200

    funkar det?????

--- Double-sensoring-WPF/YTGraphWPFUC.xaml.cs
+++ Double-sensoring-WPF/YTGraphWPFUC.xaml.cs
@@ -174,7 +174,7 @@ namespace Module_Graphs
         {
             double w = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
             double h = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
-            double scalew = w / 1376;
+            double scalew = w / 1366;
             double scaleh = h / 768;
             chart1.Width = (int)Convert.ToInt32(e.NewSize.Width * scalew);
             chart1.Height = (int)Convert.ToInt32(e.NewSize.Height * scaleh);
