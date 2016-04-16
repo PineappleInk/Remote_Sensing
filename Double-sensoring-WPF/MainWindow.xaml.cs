cc0f0361396c2e0d2e8d61c7c68dfd2161948b4d
rasgu789 <rasmusgustavsson.rg@gmail.com>
2016-04-16 14:19:34 +0200

    Utkast för beräkning av hjärtfrekvens - OBS: Ej fungerande

--- Double-sensoring-WPF/MainWindow.xaml.cs
+++ Double-sensoring-WPF/MainWindow.xaml.cs
@@ -871,7 +871,7 @@ namespace Microsoft.Samples.Kinect.BodyBasics
                         chartPulse.CheckAndAddSeriesToGraph("Pulsemarkers3", "marker_heightSorted");
                         chartPulse.ClearCurveDataPointsFromGraph();
 
-                        double average = 0;
+                        //double average = 0;
 
                         /* Toppdetektering. Toppar urskiljs även från brus. */
                         // Toppdetektering. Steg (1)
@@ -890,6 +890,39 @@ namespace Microsoft.Samples.Kinect.BodyBasics
                         //peaksByTime = sortByTime(peaksPulse);
                         peaksByTime = sortByTime(peaksAndValleysByHeight);
                         /* SLUT toppdetektering */
+
+                        //Beräkning av hjärtfrekvens
+                        Console.WriteLine("Heartrate beräkning påbörjas");
+                        double heartrate = 0;
+                        double calcHeartRateOverSeconds = 10;
+                        double periods = 0;
+                        List<double> timeBetweenHeartBeats = new List<double>();
+                        timeBetweenHeartBeats = timeBetweenAllPeaks(peaksByTime);
+                        double xStart = samplesOfMeasurement - (calcHeartRateOverSeconds * fps);
+                        Console.WriteLine("xStart: " + xStart);
+                        for (int i = 0; i < peaksByTime[0].Count - 1; ++i)
+                        {
+                            Console.WriteLine("x-count i peaksbytime: " + peaksByTime[0].Count);
+                            if (peaksByTime[0][i] >= xStart)
+                            {
+                                double T = timeBetweenHeartBeats[i];
+                                Console.WriteLine("T: " + T);
+                                double f = 1 / T;
+                                Console.WriteLine("f: " + f);
+                                double fOneMinute = f * 60;
+                                Console.WriteLine("fOneMinute: " + fOneMinute);
+                                heartrate += fOneMinute;
+                                Console.WriteLine("heartrate addition: " + heartrate);
+                                periods += 1;
+                                Console.WriteLine("periods: " + periods);
+                            }
+                        }
+                        heartrate = Math.Round(heartrate / periods);
+                        Console.WriteLine("Slutgiltig heartrate: " + heartrate);
+
+                        //Skriver ut heartrate på skärmen
+                        heartPulse = heartrate;
+                        Console.WriteLine("heartPulse: " + heartPulse);
 
                         //// OM MAN VILL HA DET SOM EN FINFIN KURVA
                         // Plottning av pulskurva (färgvärde över tid), samt alla typer av toppdetekteringar
@@ -936,16 +969,16 @@ namespace Microsoft.Samples.Kinect.BodyBasics
                         }
 
                         //Average är antalet pulsslag under 60 sekunder
-                        average = peaksPulse[0].Count() * 60 / pulseWarningInSeconds;
+                        //average = peaksPulse[0].Count() * 60 / pulseWarningInSeconds;
 
                         //Placerar uppdaterar variabel för medelvärdet, för att användas i det visuella hjärtat
-                        heartPulse = average;
+                        //heartPulse = average;
                         ////Skriver ut pulspeakar i programmet
                         //textBlockpeak.Text = "Antal peaks i puls: " + System.Environment.NewLine + peaks[0].Count()
                         //    + System.Environment.NewLine + "Uppskattad BPM: " + average;
 
                         //Tar in larmgränsen och jämför med personens uppskattade puls.
-                        pulseAlarm(average, lowNumPulse);
+                        pulseAlarm(heartrate, lowNumPulse);
 
                         for (int k = j; k < rgbFiltList.Count(); k++)
                         {
